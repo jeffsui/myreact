9083c0c4f59e002c0e75197c99546db9675b54db
Jeff.sui <Jeff.sui@fmr.com>
2016-08-16 17:17:04 +0800

    first blood

---
+++ app/component.js
@@ -0,0 +1,6 @@
+//component.js
+'use strict'
+module.exports = function(){
+    var a = '<h1>hello react world!</h1>'
+    return a;
+}
---
+++ app/main.js
@@ -0,0 +1,7 @@
+//main.js
+'use strict'
+require('!style!css!./style.css')
+var component = require('./component.js');
+
+// document.write(component());
+document.body.innerHTML=component();
